bc35b56af8719ef095c14fc0275401e7e69a7ef3
Chimipupu <chiminyanyan@gmail.com>
2025-06-03 15:19:02 +0900

    実装のベースを作成

--- doc/pj_design.md
+++ doc/pj_design.md
@@ -9,20 +9,20 @@
 『共通』
 
 - F/W ... WiFi経由でデータを送受信できること
-- OS ... ~~Amazon FreeRTOSを搭載する~~ 1週間しか工数がないため、RTOSは未搭載
+- OS ... 1週間の開発期間で工数がないため、RTOSは搭載しない
 
 - マイコン(SoC)
   - ESP32-S3-WROOM-1-N16R8
     - CPU ... Tensilica Xtensa LX7
     - Clock ... 240MHz
-    - RAM ... 520KB
+    - RAM ... 512KB
     - PSRAM ... 8MB
     - Flash ... 16MB
     - WiFi ... Wi-Fi(802.11b/g/n)@2.4GHz
 
 ```mermaid
 sequenceDiagram
-    TX(送信側) ->>+RX(受信側): WiFi通信リクエスト
+    TX(送信側) ->>+RX(受信側): WiFi通信リクエスト（ESP-NOWはMACアドレス指定）
     RX(受信側)-->>-TX(送信側): WiFi通信準備完了レスポンス
     TX(送信側) ->>+RX(受信側): LEDの色変更リクエスト
     RX(受信側)-->>-TX(送信側): 処理レスポンス(※正常系のみ)
@@ -31,18 +31,18 @@ sequenceDiagram
 『TX(送信側)』
 
 - 1)　RXに指定したLEDの色データを送信する
-- 2)　電文は「`led (RGBの色コード)`」で送る
+- 2)　電文は「`led req (RGBの色コード)`」で送る
 
 『RX(受信側)』
 
-- 1)　TXから「`led (RGBの色コード)`」を受信するまでポーリング
+- 1)　TXから「`led req (RGBの色コード)`」を受信するまでポーリング
 - 2)　TXから指定されたLEDの色データをLEDに反映
 - 3)　TXに「`led proc ok`」または「`led proc ng`」をレスポンス
 
 ### 1.1.無線関連
 
 - WiFi (IEEE802.11b/g/n)
-- 送受信 ... UDPやESPNOWを使用すること
+- 送受信 ... プロトコルにESP-NOWを使用してデータの送受信ができること
 
 ### 1.2.通信フォーマット
 
@@ -52,7 +52,7 @@ sequenceDiagram
   - 正常レスポンス：`com res ok` ... 通信準備OK
   - 異常レスポンス：`com res ng` ... (TBD)通信準備NG
 
-#### 1.2.2.led (RGBの色コード)
+#### 1.2.2.led req (RGBの色コード)
 
 - `led req (RGBの色コード)` ... 指定のLED色にするよう要求
   - 正常レスポンス：`led res ok` ... LEDの色設定OK
@@ -60,11 +60,7 @@ sequenceDiagram
 
 ## 2.ビルド構成
 
-- Arduino IDEでのビルド構成は下記図の通りである。
-
-<div align="left">
-  <img width="500" src="/doc/dd_in_work_arduinoide_build_info_20250603.png">
-</div>
+Arduino IDEでのビルド構成は下記図の通りである。
 
 - ボード ... "ESP32S3 Dev Module"
 - USB CDC On Boot ... "Disabled"
@@ -78,3 +74,7 @@ sequenceDiagram
 - JTAG Adapter ... "Integrated USB JTAG"
 - Arduino Runs On ... "Core 1"
 - USB Firmware MSC On Boot ... "Disabled"
+
+<div align="left">
+  <img width="500" src="/doc/dd_in_work_arduinoide_build_info_20250603.png">
+</div>
--- src/dd_in_work_esp32s3/dd_in_work_esp32s3.ino
+++ src/dd_in_work_esp32s3/dd_in_work_esp32s3.ino
@@ -11,10 +11,61 @@
 #include <stdint.h>
 #include <string.h>
 #include "esp_mac.h"
+#include <esp_now.h>
+#include <WiFi.h>
 
+#define DEBUG_DD_ESP // デバッグ用マクロ
+#ifdef DEBUG_DD_ESP
 static void mac_addr_print(void);
 static String get_mac_addr(esp_mac_type_t mac_type);
+#endif // DEBUG_DD_ESP
 
+static void espnow_init(void);
+
+#define DD_ESP_TX
+#ifdef DD_ESP_TX
+/**
+ * @brief ESP-NOW TXメイン関数
+ * 
+ */
+static void dd_tx_esp_main(void)
+{
+    // TODO
+}
+#endif // DD_ESP_TX
+
+// #define DD_ESP_RX
+#ifdef DD_ESP_RX
+/**
+ * @brief ESP-NOW rXメイン関数
+ * 
+ */
+static void dd_rx_esp_main(void)
+{
+    // TODO
+}
+#endif // DD_ESP_RX
+
+
+/**
+ * @brief ESP-NOWの初期化関数
+ * 
+ */
+static void espnow_init(void)
+{
+    esp_err_t ret = ESP_OK;
+
+    WiFi.mode(WIFI_STA);
+    ret = esp_now_init();
+
+    if( ret != ESP_OK){
+        Serial.println("[ERR] : ESP-NOW initialization failed");
+    }else{
+        Serial.println("[INF] : ESP-NOW initialization succeeded");
+    }
+}
+
+#ifdef DEBUG_DD_ESP
 /**
  * @brief MACアドレスを取得する関数
  * 
@@ -26,7 +77,7 @@ static String get_mac_addr(esp_mac_type_t mac_type)
     String mac = "";
 
     unsigned char mac_buf[6] = {0};
-    memset(0x00, mac_buf, sizeof(mac_buf));
+    memset(mac_buf, 0x00, sizeof(mac_buf));
 
     if (esp_read_mac(mac_buf, mac_type) == ESP_OK) {
         char buf[18] = {0};
@@ -58,17 +109,35 @@ static void mac_addr_print(void)
     Serial.print("Ethernet MAC\t\t\t");
     Serial.println(get_mac_addr(ESP_MAC_ETH));
 }
+#endif
 
 void setup()
 {
     Serial.begin(115200);
+
     while (!Serial) {
         delay(100);
     }
+
+#if defined(DD_ESP_TX)
+    Serial.println("ESP_TX");
+#elif defined(DD_ESP_RX)
+    Serial.println("ESP_RX");
+#endif
+
+    espnow_init();
+
+#ifdef DEBUG_DD_ESP
+    mac_addr_print();
+    delay(5000);
+#endif
 }
 
 void loop()
 {
-    get_mac_addr();
-    delay(5000);
+#if defined(DD_ESP_TX)
+    dd_tx_esp_main();
+#elif defined(DD_ESP_RX)
+    dd_rx_esp_main();
+#endif
 }
